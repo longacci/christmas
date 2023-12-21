ab2c097a792b98fb39ebcc0334ce11f2b3f99926
long <hailongavatar1@gmail.com>
2023-12-21 17:49:43 +0700

    fix audio

--- 1.js
+++ 1.js
@@ -1,9 +1,14 @@
-window.addEventListener('DOMContentLoaded',() => {
+window.addEventListener('load',() => {
     const button = document.querySelector(".button");
     const title = document.querySelector(".title");
     const star = document.querySelector(".star");
     const tree = document.querySelector(".tree");
     const image = document.querySelector(".box_image");
+    const banner = document.querySelector(".banner");
+    const audio = document.createElement("AUDIO");
+    audio.src = `./audio.mp3`;
+    banner.insertAdjacentElement("beforebegin",audio);
+    audio.autoplay = true;
     button.addEventListener("click",()=> {
         button.classList.add("hidden_button");
         title.classList.add("hidden_title");
